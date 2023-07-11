556f844da4dff3f6c8d1a8aed2e30f2912235b51
malerinc <malerinc>
2023-07-11 16:40:04 +0200

    Fixed controls setup.

--- fabricatio-controls/setup.py
+++ fabricatio-controls/setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup, find_packages
-# import unittest
 
-
-with open("MANIFEST.in", "w") as f:
-    f.write("recursive-include . *")
-
-# with open("README.md", 'r') as f:
-#     long_description = f.read()
-
+try:
+    import fabricatio_rl
+except ModuleNotFoundError:
+    print("Before installing this package, you must first install fabricatio-rl!")
+    quit()
 
 setup(
-    name='fabricatio-rl',
+    name='fabricatio-controls',
     version='1.0.0',
     python_requires='>3.6.8',
     install_requires=[
-        'fabricatio-rl==1.0.0', 'tensorflow==2.3.0', 'tensorboard==2.3.0',
-        'torch==1.10.2', 'scipy==1.5.4', 'pandas==1.1.1', 'seaborn==0.11.2',
+        'tensorflow==2.6.2', 'torch==1.10.2', 
+        'scipy==1.5.4', 'pandas==1.1.1', 'seaborn==0.11.2',
         'pathos==0.2.6', 'ortools==7.8.7959', 'stable-baselines3==1.1.0'
     ],
     description="Scheduling algorithms for FabricatioRL.",
-    # long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=find_packages('fabricatio_rl'),
+    packages=find_packages(),
     include_package_data=True,
 )
