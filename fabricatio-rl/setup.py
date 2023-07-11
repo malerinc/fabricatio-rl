91cac5bedec2377f27636608af21fb09a581d826
malerinc <malerinc>
2023-07-11 16:39:07 +0200

    Fixed simulation setup script and removed deprecated readme.

--- fabricatio-rl/setup.py
+++ fabricatio-rl/setup.py
@@ -1,19 +1,33 @@
+import os
+
 from setuptools import setup, find_packages
-import unittest
+# import unittest
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+visualization_app_paths = package_files('./fabricatio_rl/visualization')
 
 
 with open("MANIFEST.in", "w") as f:
-    f.write("recursive-include fabricatio_rl/visualization *")
+    f.write("include")
+    f.write(" ".join(visualization_app_paths))
+
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
-
-def fabricatio_test_suite():
-    # TODO: move to pytest ;)
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
+# def fabricatio_test_suite():
+#     # TODO: move to pytest ;)
+#     test_loader = unittest.TestLoader()
+#     test_suite = test_loader.discover('tests', pattern='test_*.py')
+#     return test_suite
 
 
 setup(
@@ -21,14 +35,15 @@ setup(
     version='1.0.0',
     python_requires='>3.6.8',
     install_requires=[
-      'gym==0.18.3', 'numpy==1.18.5', 'pandas==1.1.1', 'scipy==1.4.1',
+      'gym==0.18.3', 'numpy>=1.18.5', 'pandas==1.1.1', 'scipy==1.5.4',
       'Flask==1.1.1', 'Flask-RESTful==0.3.8'
     ],
     description="An Event Discrete Simulation Framework for "
                 "Production Scheduling Problems.",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=find_packages('fabricatio_rl'),
-    include_package_data=True,
-    test_suite='setup.fabricatio_test_suite'
+    packages=find_packages(),
+    package_data={'': visualization_app_paths},
+    include_package_data=True #,
+    # test_suite='setup.fabricatio_test_suite'
 )
